2fbfebe595c511eefe5fffb60eda8489b3a96cfa
Samuel <sherrero@buguroo.com>
2014-11-06 17:30:06 +0100

    PASO 3. endpoints se escriben a YAML con la documentación de la API de Trello. Se inyecta dicha documentación como doctring en los métodos.

--- api.py
+++ api.py
@@ -1,64 +1,127 @@
-import requests
+#!/usr/bin/env python
+"""
+APIs de Trello.
+
+"""
 from functools import partial
-from endpoints import ENDPOINTS
+import os
+import gzip
+from base64 import b64decode
+
+import requests
+import yaml
 
 TRELLO_URL = 'https://trello.com/'
+HERE = os.path.dirname(__file__)
+
+with open(os.path.join(HERE, 'endpoints.yaml'), 'rb') as ep_file:
+    ENDPOINTS = yaml.load(ep_file.read())
 
 
 class TrelloAPI:
-    def __init__(self, endpoints, name, apikey, parent=None):
-        self._ep = endpoints
-        self._arg = None
-        self._parent = parent
+    """
+    Clase encargada de hacer de interfaz con la API de Trello.
+
+    Genera métodos dinámicamente que se corresponden con las distintas
+    ramas de las URLs.
+
+    Define el método __call__ para poder pasar parámetros en la URL.
+
+    """
+    def __init__(self, endpoints, name, apikey, parent=None, api_arg=None):
+        self._endpoints = endpoints
         self._name = name
         self._apikey = apikey
-        for name, content in self._ep.items():
+        self._parent = parent
+        self._api_arg = api_arg
+
+        self._allowed_args = []
+
+        for name, content in self._endpoints.items():
             if name == 'METHODS':
-                for method in content:
-                    verb = method.lower()
-                    setattr(self, verb, partial(self._api_call, verb))
+                # Métodos HTTP de este endpoint.
+                for api_method, doc in content:
+                    name = api_method.lower()
+                    obj_method = partial(self._api_call, name)
+                    obj_method.__doc__ = self._unpack_doc(doc)
+                    setattr(self, name, obj_method)
+            elif name.startswith('_') and name.endswith('_'):
+                # Argumento de la API
+                self._allowed_args.append(name.strip('_'))
             else:
-                setattr(self, name,
-                        TrelloAPI(self._ep[name], name, self._apikey, self))
+                # Path parcial de la API.
+                next_path = TrelloAPI(endpoints=self._endpoints[name],
+                                      name=name,
+                                      apikey=self._apikey,
+                                      parent=self)
+                setattr(self, name, next_path)
+
+    @staticmethod
+    def _unpack_doc(b64):
+        return gzip.decompress(b64decode(b64)).decode('utf-8')
 
     @property
     def _url(self):
         """
         Resuelve la URL hasta este punto.
 
-        >>> trello = TrelloAPI(ENDPOINTS['TrelloV1'], '1', 'APIKEY')
+        >>> trello = TrelloAPI(ENDPOINTS['1'], '1', 'APIKEY')
         >>> trello.batch._url
         '1/batch'
-        >>> trello.boards('BOARD_ID')._url
+        >>> trello.boards(board_id='BOARD_ID')._url
         '1/boards/BOARD_ID'
-        >>> trello.boards('BOARD_ID')('FIELD')._url
+        >>> trello.boards(board_id='BOARD_ID')(field='FIELD')._url
         '1/boards/BOARD_ID/FIELD'
-        >>> trello.boards('BOARD_ID').cards('FILTER')._url
+        >>> trello.boards(board_id='BOARD_ID').cards(filter='FILTER')._url
         '1/boards/BOARD_ID/cards/FILTER'
 
         """
-        mypart = '/'.join(filter(None, [self._name, self._arg]))
+        if self._api_arg:
+            mypart = str(self._api_arg)
+        else:
+            mypart = self._name
 
         if self._parent:
             return '/'.join(filter(None, [self._parent._url, mypart]))
         else:
             return mypart
 
-    def _api_call(self, method, *args, **kwargs):
-        if 'params' in kwargs:
-            kwargs['params']['key'] = self._apikey
+    def _api_call(self, method_name, *args, **kwargs):
+        """
+        Hace la petición HTTP al endpoint deseado.
+
+        """
+        kwargs.setdefault('params', {}).update({'key': self._apikey})
+
+        http_method = getattr(requests, method_name)
+        return http_method(TRELLO_URL + self._url, *args, **kwargs)
+
+    def __call__(self, **kwargs):
+        """
+        Añade un parámetro variable a la URL de la API.
+
+        Se pueden consultar los parámetros disponibles mirando en
+        `_allowed_args`.
+
+        """
+        if not kwargs:
+            raise ValueError("A keyword argument must be provided: {}".format(
+                                ', '.join(self._allowed_args)))
+        elif len(kwargs) > 1:
+            raise ValueError("Too many arguments.")
+        elif set(kwargs.keys()) <= set(self._allowed_args):
+            _name, _api_arg = list(kwargs.items())[0]
+            name = '_' + _name + '_'
+            return TrelloAPI(endpoints=self._endpoints[name],
+                             name=name,
+                             apikey=self._apikey,
+                             parent=self,
+                             api_arg=_api_arg)
         else:
-            kwargs['params'] = {'key': self._apikey}
+            raise ValueError("Unknown argument {}".format(kwargs.keys()))
 
-        method = getattr(requests, method)
-
-        return method(TRELLO_URL + self._url, *args, **kwargs)
-
-    def __call__(self, arg):
-        self._arg = str(arg)
-
-        return TrelloAPI(self._ep, None, self._apikey, self)
-
+    def __repr__(self):
+        return 'TrelloQuery <"{}">'.format(self._url)
 
 if __name__ == '__main__':
-    TrelloV1 = TrelloAPI(ENDPOINTS['TrelloV1'], '1', 'INSERT_KEY_HERE')
+    TrelloV1 = TrelloAPI(ENDPOINTS['1'], '1', 'INSERT_KEY_HERE')
--- make_endpoints.py
+++ make_endpoints.py
@@ -4,15 +4,23 @@ Convierte la documentación de Trello en un módulo de python que contiene
 un diccionario anidado con los endpoints y métodos que acepta la API.
 
 """
+from base64 import b64encode
 from collections import defaultdict
 from lxml import html
-from pprint import pprint
+import gzip
+import re
 
+from html2text import html2text
+from lxml import etree
 import requests
+import yaml
 
-TRELLO_API_DOC = 'https://trello.com/docs/api/index.html'
+TRELLO_API_DOC = 'https://trello.com/docs/api/'
 HTTP_METHODS = {'OPTIONS', 'GET', 'HEAD', 'POST', 'PUT', 'DELETE',
                 'TRACE', 'CONNECT'}
+EP_DESC_REGEX = re.compile(
+    '.*({methods})\s([\/a-zA-Z0-9\[\]\s_]+).*'.format(
+        methods='|'.join(HTTP_METHODS)))
 
 def _is_url_arg(p):
     """
@@ -49,13 +57,13 @@ def _camelcase_to_underscore(url):
 
     """
     def upper2underscore(text):
-        for letter in text:
-            if letter.islower():
-                yield letter
+        for char in text:
+            if char.islower():
+                yield char
             else:
                 yield '_'
-                if letter.isalpha():
-                    yield letter.lower()
+                if char.isalpha():
+                    yield char.lower()
     return ''.join(upper2underscore(url))
 
 
@@ -63,30 +71,34 @@ def create_tree(endpoints):
     """
     Crea el árbol de endpoints de Trello.
 
-    >>> r = {'TrelloV1': { \
-                 'actions': {'METHODS': {'GET'}}, \
-                 'boards': { \
-                     'members': {'METHODS': {'DELETE'}}}} \
-            }
+    >>> r = {'1': { \
+                'boards': { \
+                    '_board_id_': { \
+                        'members': { \
+                            '_id_member_': {'METHODS': [['DELETE', 'doc2']]} \
+                        } \
+                    } \
+                }, \
+                'actions': { \
+                   '_id_action_': {'METHODS': [['GET', 'doc1']]} \
+                } \
+            }}
     >>> r == create_tree([ \
-                 'GET /1/actions/[idAction]', \
-                 'DELETE /1/boards/[board_id]/members/[idMember]'])
+                 ('GET', '/1/actions/[idAction]', 'doc1'), \
+                 ('DELETE', '/1/boards/[board_id]/members/[idMember]', 'doc2')])
     True
 
     """
     tree = {}
 
-    for ep in endpoints:
-        # 'GET /1/actions/[idAction]' => ['GET', ['1', 'actions']]
-        verb, url = ep.split(' ', 1)
-        path = [p for p in url.strip('/').split('/')
-                if not _is_url_arg(p)]
+    for method, url, doc in endpoints:
+        path = [p for p in url.strip('/').split('/')]
         here = tree
 
         # Primer elemento (Versión de la API).
-        cls_name = 'TrelloV' + path[0]
-        here.setdefault(cls_name, {})
-        here = here[cls_name]
+        version = path[0]
+        here.setdefault(version, {})
+        here = here[version]
 
         # Resto de elementos de la URL.
         for p in path[1:]:
@@ -96,27 +108,43 @@ def create_tree(endpoints):
 
         # Métodos HTTP admitidos.
         if not 'METHODS' in here:
-            here['METHODS'] = {verb}
+            here['METHODS'] = [[method, doc]]
         else:
-            here['METHODS'].add(verb)
+            if not method in here['METHODS']:
+                here['METHODS'].append([method, doc])
 
     return tree
 
 
 def main():
     """
-    Convierte la documentación de Trello en un diccionario y lo imprime
-    por salida estándar.
+    Convierte la documentación de Trello en una estructura de datos y la
+    imprime por salida estándar.
 
     """
     ep = requests.get(TRELLO_API_DOC).content
     root = html.fromstring(ep)
 
-    links = root.xpath('//a/text()')
-    endpoints = [ep for ep in links if _is_api_definition(ep)]
+    links = root.xpath('//a[contains(@class, "reference internal")]/@href')
+    pages = [requests.get(TRELLO_API_DOC + u)
+             for u in links if u.endswith('index.html')]
 
-    print('ENDPOINTS = ', end='')
-    pprint(create_tree(endpoints), compact=True)
+    endpoints = []
+    for page in pages:
+        root = html.fromstring(page.content)
+        sections = root.xpath('//div[@class="section"]/h2/..')
+        for sec in sections:
+            ep_html = etree.tostring(sec).decode('utf-8')
+            ep_text = html2text(ep_html).splitlines()
+            match = EP_DESC_REGEX.match(ep_text[0])
+            if not match:
+                continue
+            ep_method, ep_url = match.groups()
+            ep_text[0] = ' '.join([ep_method, ep_url])
+            ep_doc = b64encode(gzip.compress('\n'.join(ep_text).encode('utf-8')))
+            endpoints.append((ep_method, ep_url, ep_doc))
+
+    print(yaml.dump(create_tree(endpoints)))
 
 
 if __name__ == '__main__':
